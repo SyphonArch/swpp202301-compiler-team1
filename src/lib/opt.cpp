0ebbc709923c2cd5330f032e58319f1f5a77cd58
Jake Hyun <jake.hyun@hotmail.com>
2023-05-07 00:41:33 +0900

    [Sprint 1] [A-VII] Combine Adds into Sum (#19)
    * Initial implementation
    
    * Critical fix: reverse instruction traversal order
    
    * Fix logic to better optimize
    
    * Add unit tests
    
    * Fix flaw in logic
    
    * Add comments
    
    * Update misleading comments
    
    * Fix opcode related bug and refactor nested code
    
    * Trivial: fix comment typos
    
    * Fix incomplete test
    
    * Add add_nine test
    
    * Add unit tests
    
    * Match formatting
    
    * Convert redundant `if` into assertion
    
    * Remove redundant header inclusions
    
    * Implement `mul` expansion
    
    * Implement non-one-use `add` expansion
    
    * Trivial: move comment location
    
    * Include `sub` optimization
    
    * Trivial: change test and add comments
    
    * Fix faulty test
    
    * Modify tests to have noundef values only
    
    * Implement constant merging
    
    * Fix weight calculation logic to correctly consider constant merging
    
    * Additional fix for correct weight calculation
    
    * Remove unreachable logic
    
    * Trivial: remove redundant comment

--- src/lib/opt.cpp
+++ src/lib/opt.cpp
@@ -5,6 +5,8 @@
 
 #include "print_ir.h"
 
+#include "./opt/add_to_sum.h"
+
 using namespace std::string_literals;
 
 namespace sc::opt {
@@ -25,6 +27,7 @@ optimizeIR(std::unique_ptr<llvm::Module> &&__M,
     // Add loop-level opt passes below
 
     // Add function-level opt passes below
+    FPM.addPass(add_to_sum::AddToSum());
 
     CGPM.addPass(llvm::createCGSCCToFunctionPassAdaptor(std::move(FPM)));
     // Add CGSCC-level opt passes below
