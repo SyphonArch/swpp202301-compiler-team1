c3fec25ff12ee7aaea2ff12372758d084dd4d952
Jake Hyun <jake.hyun@hotmail.com>
2023-05-18 11:08:36 +0900

    Enforce LCSSA

--- src/lib/opt/loop_unrolling.cpp
+++ src/lib/opt/loop_unrolling.cpp
@@ -7,6 +7,7 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Transforms/Utils/UnrollLoop.h"
+#include "llvm/Transforms/Utils/LCSSA.h"
 
 using namespace llvm;
 
@@ -14,20 +15,26 @@ namespace sc::opt::loop_unrolling {
 
 PreservedAnalyses LoopUnrolling::run(Function &F,
                                      FunctionAnalysisManager &FAM) {
+
+  LCSSAPass lcssa_pass;
+  lcssa_pass.run(F, FAM);
+
   LoopInfo &LI = FAM.getResult<LoopAnalysis>(F);
   ScalarEvolution &SE = FAM.getResult<ScalarEvolutionAnalysis>(F);
   DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
 
   for (Loop *L : LI) {
-    // Determine the loop unrolling options based on your requirements
-    UnrollLoopOptions ULO{8, true, true, true, true, true};
+    if (L->isInnermost() && L->isLCSSAForm(DT)) {
+      // Determine the loop unrolling options based on your requirements
+      UnrollLoopOptions ULO{8, true, true, true, true, true};
 
-    // Set the remaining parameters as needed
-    Loop *RemainderLoop = nullptr;
-    bool PreserveLCSSA = false;
+      // Set the remaining parameters as needed
+      Loop *RemainderLoop = nullptr;
+      bool PreserveLCSSA = false;
 
-    // Perform loop unrolling using UnrollLoop function
-    LoopUnrollResult Result = UnrollLoop(L, ULO, &LI, &SE, &DT, nullptr, nullptr, nullptr, PreserveLCSSA);
+      // Perform loop unrolling using UnrollLoop function
+      LoopUnrollResult Result = UnrollLoop(L, ULO, &LI, &SE, &DT, nullptr, nullptr, nullptr, PreserveLCSSA);
+    }
   }
 
   return PreservedAnalyses::none();
