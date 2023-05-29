6be3c8ffbea94c97a6647f478c63773998afc4a4
Jake Hyun <jake.hyun@hotmail.com>
2023-05-29 23:49:52 +0900

    Implement definition-usage distance reduction

--- src/lib/opt/register_pressure_reduction.cpp
+++ src/lib/opt/register_pressure_reduction.cpp
@@ -10,6 +10,57 @@ using namespace std;
 namespace sc::opt::register_pressure_reduction {
 PreservedAnalyses RegisterPressureReduction::run(Function &F,
                                                  FunctionAnalysisManager &FAM) {
+
+  // Collect all instructions which don't have side effects
+  std::vector<Instruction *> instructions;
+  // Record the location of Instructions within their respective blocks
+  DenseMap<Instruction *, int> loc_in_block;
+  for (BasicBlock &BB : F) {
+    int loc_in_block_tracker = 0;
+    for (Instruction &I : BB) {
+      if (!I.mayHaveSideEffects()) {
+        instructions.push_back(&I);
+      }
+      loc_in_block[&I] = loc_in_block_tracker++;
+    }
+  }
+
+  // Looping over the previously collected instructions,
+  for (auto I : instructions) {
+    // Collect all usages of the instruction in the same block.
+    int curr_loc = loc_in_block[I];
+    Instruction *first_usage = nullptr;
+    int usage_loc;
+    bool has_usage_in_block = false;
+    for (auto user : I->users()) {
+      if (auto usage_inst = dyn_cast<Instruction>(user)) {
+        if (usage_inst->getParent() == I->getParent()) {
+          has_usage_in_block = true;
+          usage_loc = loc_in_block[usage_inst];
+          if (usage_loc > curr_loc && (first_usage == nullptr ||
+                                       usage_loc < loc_in_block[first_usage])) {
+            first_usage = usage_inst;
+          }
+        }
+      }
+      if (first_usage != nullptr) {
+        // TODO: Resolve issue with out-of-block Instruction moving
+        //I->moveBefore(first_usage);
+      }
+    }
+
+    if (!has_usage_in_block) {
+      // If there aren't any usages in the same block
+      if (I->hasOneUse()) {
+        User *only_user = *I->user_begin();
+        if (auto only_inst = dyn_cast<Instruction>(only_user)) {
+          outs() << "Moving " << I->getName() << " to " << only_inst->getName() << "\n";
+          // TODO: Resolve issue with out-of-block Instruction moving
+          //I->moveBefore(only_inst);
+        }
+      }
+    }
+  }
   return PreservedAnalyses::none();
 }
 
